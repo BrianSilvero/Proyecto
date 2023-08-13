2217951b3f60f1a7b76feaa4127b5d9dc3b1b2e1
Brian Silvero <briandavidsilvero@gmail.com>
2023-08-13 01:02:39 -0300

    Aplicando java

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ class Carrito {
   constructor() {
     this.listaCarrito = [];
   }
+
+  agregar(producto){
+    this.listaCarrito.push(producto)
+  }
+
+  mostrar(){
+    let contenedor_carrito = document.getElementById('contenedor_carrito')
+    contenedor_carrito.innerHTML = ""
+    this.listaCarrito.forEach(producto => {
+        contenedor_carrito.innerHTML += `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${producto.img}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">Precio: $${producto.precio}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    })
+  }
 }
 
 class controladorDeProducto{
@@ -33,18 +58,26 @@ class controladorDeProducto{
                 <h5 class="card-title">${producto.nombre}</h5>
                 <p class="card-text">${producto.descripcion}</p>
                 <p class="card-text">$${producto.precio}</p>
-                <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                <a href="#" class="btn btn-primary" id="ap-${producto.id}">Añadir al carrito</a>
             </div>
           </div>`
+        })
+
+        this.listaDeProductos.forEach( producto => {
+            const btn = document.getElementById(`ap-${producto.id}`)
+            btn.addEventListener("click",() =>{
+                carrito.agregar(producto)
+                carrito.mostrar()
+            })
         })
     }
 }
 
 // Productos del sitio
-const Agenda = new Producto (1, "Agenda", 2300, "Diseñada para satisfacer tus necesidades en un mundo lleno de actividades, la Agenda Hunger combina estilo y funcionalidad en un elegante formato. Con páginas de alta calidad, secciones personalizables y un diseño intuitivo, esta agenda te ayudará a conquistar tus metas día tras día. ¡Eleva tu productividad y estilo con la Agenda Hunger", "https://encycolorpedia.es/gimp.svg")
-const Remera = new Producto (2, "Remera", 3800, "Fabricada con materiales de primera calidad, esta remera es el equilibrio perfecto entre estilo y confort. Ya sea para el día a día o para destacar en tus actividades, la Remera Hunger te acompaña con su diseño moderno y ajuste impecable. ¡Eleva tu estilo con la autenticidad de Hunger!", "assets\agenda.png")
-const Taza = new Producto (3, "Taza", 1500, "Creada con pasión y calidad premium, esta taza es mucho más que un recipiente, es un compañero de emociones. Cada café, té o chocolate caliente se transforma en un ritual con su diseño cautivador y el confort de sostenerla en tus manos. ¡Eleva tus mañanas y despierta tus sentidos con la Taza Hunger, la chispa que transforma tus bebidas en momentos inolvidables!", "assets/agenda.png")
-const Botella = new Producto (4, "Botella", 2000, "Diseñada para quienes buscan explorar el mundo, esta botella es resistente, funcional y llena de estilo. Mantén tus bebidas frías o calientes durante horas, mientras te desplazas con confianza gracias a su construcción de alta calidad. Siente la sed de la aventura y mantente hidratado con la Botella Hunger, tu compañera perfecta en cada camino que decidas recorrer.", "assets/agenda.png")
+const Agenda = new Producto (1, "Agenda", 2300, "Diseñada para satisfacer tus necesidades en un mundo lleno de actividades, la Agenda Hunger combina estilo y funcionalidad en un elegante formato. Con páginas de alta calidad, secciones personalizables y un diseño intuitivo, esta agenda te ayudará a conquistar tus metas día tras día. ¡Eleva tu productividad y estilo con la Agenda Hunger", "https://raw.githubusercontent.com/BrianSilvero/Proyecto/main/assets/agenda.png")
+const Remera = new Producto (2, "Remera", 3800, "Fabricada con materiales de primera calidad, esta remera es el equilibrio perfecto entre estilo y confort. Ya sea para el día a día o para destacar en tus actividades, la Remera Hunger te acompaña con su diseño moderno y ajuste impecable. ¡Eleva tu estilo con la autenticidad de Hunger!", "https://raw.githubusercontent.com/BrianSilvero/Proyecto/main/assets/remera.png")
+const Taza = new Producto (3, "Taza", 1500, "Creada con pasión y calidad premium, esta taza es mucho más que un recipiente, es un compañero de emociones. Cada café, té o chocolate caliente se transforma en un ritual con su diseño cautivador y el confort de sostenerla en tus manos. ¡Eleva tus mañanas y despierta tus sentidos con la Taza Hunger, la chispa que transforma tus bebidas en momentos inolvidables!", "https://raw.githubusercontent.com/BrianSilvero/Proyecto/main/assets/taza.png")
+const Botella = new Producto (4, "Botella", 2000, "Diseñada para quienes buscan explorar el mundo, esta botella es resistente, funcional y llena de estilo. Mantén tus bebidas frías o calientes durante horas, mientras te desplazas con confianza gracias a su construcción de alta calidad. Siente la sed de la aventura y mantente hidratado con la Botella Hunger, tu compañera perfecta en cada camino que decidas recorrer.", "https://raw.githubusercontent.com/BrianSilvero/Proyecto/main/assets/botella.png")
 
 
 // Carrito del sitio
